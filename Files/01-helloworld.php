c61a5f877c0609627c225a820de55db15544499e
Pedro Kielblock <p.kielblock123@gmail.com>
2022-02-01 23:55:52 -0300

    Hello World

--- Files/01-helloworld.php
+++ Files/01-helloworld.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
 <?php
+    echo ("<h1>Hello World</h1>") . PHP_EOL;
+?>
+</body>
+</html>
