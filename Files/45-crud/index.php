9400a33b510c155d5c1834b1f904e972e22ca52d
Pedro Kielblock <p.kielblock123@gmail.com>
2022-04-14 23:48:45 -0300

    Update

--- Files/45-crud/index.php
+++ Files/45-crud/index.php
@@ -1,6 +1,5 @@
 <?php
-require('db/connection.php');
-
+    require('db/connection.php');
 ?>
 <!doctype html>
 <html lang="pt-BR">
@@ -19,11 +18,14 @@ require('db/connection.php');
             text-align: center;
             border: 1px solid black;
         }
+        .oculto{
+            display: none;
+        }
     </style>
 </head>
 <body>
     <h1>Inserindo Dados</h1>
-    <form method="post">
+    <form id="formSalva" method="post">
         <label>
             Digite Seu Nome: <input type="text" name="nome" placeholder="Nome" required>
         </label>
@@ -31,6 +33,17 @@ require('db/connection.php');
             Digite Seu Email: <input type="email" name="email" placeholder="Email" required>
         </label>
         <button type="submit" name="salvar">Salvar</button>
+    </form>
+    <form class="oculto" id="formAtualiza" method="post">
+        <input type="hidden" id="idEditado" name="idEditado" placeholder="ID" required>
+        <label>
+            Digite Novo Nome: <input type="text" id="nomeEditado" name="nomeEditado" placeholder="Nome" required>
+        </label>
+        <label>
+            Digite Novo Email: <input type="email" id="emailEditado" name="emailEditado" placeholder="Email" required>
+        </label>
+        <button type="submit" name="atualizar">Atualizar</button>
+        <button type="submit" id="voltar" name="voltar">Voltar</button>
     </form>
     <br>
     <?php
@@ -72,6 +85,38 @@ require('db/connection.php');
     }
     ?>
     <?php
+        //Processo de atualização
+        if(isset($_POST['atualizar']) && isset($_POST['idEditado']) && isset($_POST['nomeEditado'])  && isset($_POST['emailEditado'])){
+            $id = limparPost($_POST['idEditado']);
+            $nome = limparPost($_POST['nomeEditado']);;
+            $email = limparPost($_POST['emailEditado']);;
+
+            //Validating empty fields
+            if($nome == "" || $nome == null){
+                echo "<b style='color:red'>Nome não pode ser vazio</b>" . PHP_EOL;
+                exit();
+            }
+            if($email == "" || $email == null){
+                echo "<b style='color:red'>Email não pode ser vazio</b>" . PHP_EOL;
+                exit();
+            }
+            //Validating Name and Email
+            if (!preg_match("/^[a-zA-Z-' ]*$/", $nome)) {
+                echo "<b style='color:red'>Somente letras e espaços são permitidos</b>" . PHP_EOL;
+                exit();
+            }
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo "<b style='color:red'>Formato de Email Inválido</b>" . PHP_EOL;
+                exit();
+            }
+
+            $sql = $pdo->prepare("UPDATE clientes SET nome=?, email=? WHERE id=?");
+            $sql->execute(array($nome,$email,$id));
+
+            echo "Atualizado " . $sql->rowCount() . " registros" . PHP_EOL;
+        }
+    ?>
+    <?php
         //SELECT data
         $sql = $pdo->prepare("SELECT * FROM clientes");
         $sql->execute();
@@ -91,6 +136,7 @@ require('db/connection.php');
                     <th>ID</th>
                     <th>Nome</th>
                     <th>Email</th>
+                    <th>Ações</th>
                 </tr>
             ";
 
@@ -100,6 +146,7 @@ require('db/connection.php');
                     <td>" . $valor['id'] . "</td>
                     <td>" . $valor['nome'] . "</td>
                     <td>" . $valor['email'] . "</td>
+                    <td><a href='#' class='btnAtualizar' data-id='" . $valor['id'] . "' data-nome='" . $valor['nome'] . "' data-email='" . $valor['email'] . "'>Atualizar</a></td>
                 </tr>
                 ";
             }
@@ -109,5 +156,27 @@ require('db/connection.php');
             echo "<p>Nenhum Cliente Cadastrado</p>";
         }
     ?>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(".btnAtualizar").click(function (){
+            let id = $(this).attr('data-id');
+            let nome = $(this).attr('data-nome');
+            let email = $(this).attr('data-email');
+
+            $("#formSalva").addClass('oculto');
+            $("#formAtualiza").removeClass('oculto');
+
+            $("#idEditado").val(id);
+            $("#nomeEditado").val(nome);
+            $("#emailEditado").val(email);
+
+            //alert('O ID é: ' + id + " | Nome é: "  + nome + " | Email é " + email);
+        });
+
+        $('#voltar').click(function (){
+            $("#formSalva").removeClass('oculto');
+            $("#formAtualiza").addClass('oculto');
+        });
+    </script>
 </body>
 </html>
